23ce004b3ce18debd480d8675b1c9e7fb97ffebf
Kamiklza <kamiklza@hotmail.com>
2021-08-26 17:49:57 +0800

    final commit

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from selenium import webdriver
 
 chrome_driver_path = "/Users/Kamiklza/PycharmProjects/chromedriver"
+desktop_driver_path = "C:/Users/user/PycharmProjects/chromedriver.exe"
 
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver = webdriver.Chrome(executable_path=desktop_driver_path.encode())
+
 driver.get("https://www.python.org/")
 
-event_list = driver.find_elements_by_css_selector(".shrubbery .menu li a")
+event_time = driver.find_elements_by_css_selector(".medium-widget.event-widget.last .menu time")
+event_list = driver.find_elements_by_css_selector(".medium-widget.event-widget.last .menu li a")
 
-for even in event_list:
-    print(even.text)
+
+events = {n: {"time": event_time[n].text, "name": event_list[n].text} for n in range(len(event_time))}
+print(events)
+
+# #-------Official Answer to Create the List---------#
+# events = {}
+# for n in range(len(event_time)):
+#     events[n] = {
+#         "time" : event_time[n].text,
+#         "name" : event_list[n].text
+#     }
+# print(events)
+
+#----------------------------------------------------#
+
 
 driver.quit()
+
